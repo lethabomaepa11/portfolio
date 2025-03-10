39b0250efd101a343650ff4688d570f2af9890a8
lethabo <shysnylethabo11@gmail.com>
2025-03-10 13:26:10 +0200

    update: stay awake fix

--- netlify/functions/keep-alive.js
+++ netlify/functions/keep-alive.js
@@ -1,20 +1,22 @@
-export const handler = async (req) => {
-    const { next_run } = await req.json()
+export const handler = async ({locals:{ supabase }}) => {
     try {
         const url = 'https://lethabomaepa.netlify.app/api/getPortfolio';
-
         const response = await fetch(url, { method: "GET" });
         const res = await response.json();
-        if (!res.success) throw new Error("Keep-alive failed");
+        //also just call from the supabase api
+        const { data, error } = await supabase
+            .from('info')
+            .select('*');
+        if (error) throw new Error(error);
       
       return {
         statusCode: 200,
         body: JSON.stringify({ message: "Supabase kept alive successfully" })
       };
-    } catch (error) {
+    } catch (e) {
       return {
         statusCode: 500,
-        body: JSON.stringify({ error: error.message })
+        body: JSON.stringify({ error: e.message })
       };
     }
   };
